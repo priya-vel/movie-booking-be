35e6382658645eaf8d34460ed73045d08f3a9aea
priya-vel <priyavelmurugan761@gmail.com>
2021-11-27 15:11:44 +0530

    adding email support and excryption

--- config/index.js
+++ config/index.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 const Config = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
-    SECRET: process.env.SECRET
+    SECRET: process.env.SECRET,
+    EMAIL: process.env.EMAIL,
+    PASS_IV: process.env.PASS_IV,
+    PASS_DATA: process.env.PASS_DATA,
+    PASS_KEY: process.env.PASS_KEY,
 }
 
 
---
+++ utils/mailer.js
@@ -0,0 +1,38 @@
+"use strict";
+const nodemailer = require("nodemailer");
+require("dotenv").config()
+const { Config } = require("../config");
+const { decrypt } = require("./encrypt");
+const auth = {
+    user: decrypt(Config.SECRET, Config.EMAIL), // generated ethereal user
+    pass: decrypt(Config.SECRET, Config.PASS_DATA), // generated ethereal password
+  }
+
+  console.log(auth);
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendMaill(to, subject, message) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth,
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'Show Booking', // sender address
+    to: to, // list of receivers
+    subject, // Subject line
+    text: message, // plain text body
+    // html: "<b>Hello world?</b>", // html body
+  });
+  return info
+}
+
+module.exports = {
+    sendMaill
+}
+
